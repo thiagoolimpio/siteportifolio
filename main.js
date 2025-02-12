b954ef8cfd6d268c6865d5fc8bdb6032953e4e05
thiago <olimpiodeoliveirathiago@gmail.com>
2025-02-12 11:01:43 -0300

    teste link gmail

---
+++ main.js
@@ -0,0 +1,13 @@
+function fazGet(url){
+    let request = new XMLHttpRequest();
+    request.open("GET", url, true);
+    request.send();
+}
+
+function criaLinha(usuario) {
+
+}
+
+function main( ) {
+    usuarios = fazGet()
+}
